5081a4b59321ab7e820e216f0033a15e363ec35c
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-12-12 23:01:58 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+// Write your code in this file
+let currentUser = "Grace Hopper"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+console.log(excitedWelcomeMessage)
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
